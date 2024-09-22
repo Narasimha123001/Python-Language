307449844c286aa82875e2ad56023f10619f3505
Narasimha <narasimhaanguluri001@gmail.com>
2024-09-22 22:46:38 +0530

    guess the three digits number in given chances

---
+++ projects/Guess_number.py
@@ -0,0 +1,33 @@
+from random import randint
+
+def guess_number():
+    while True:
+        n=str(randint(100,999))
+        if not (n[0]==n[1] or n[0]==n[2] or n[1]==n[2]):
+            return n
+        
+name=input("Enter your name: ")
+print("Hi,",name,"Start the game")
+bulls=0
+cows=0
+chances =10
+num =str(guess_number())
+
+while chances > 0:
+    n=str(input("Enter your number: "))
+    if n == num:
+        print("Great! you are brillient")
+        break
+    else: 
+        for i in range(0,3):
+            if n[i] == num[i]:
+                cows =cows+1
+            elif n[i] in num:
+                
+                bulls=bulls+1
+        print("you have ",cows,"cows and",bulls,"bulls.")
+        print("you have ",chances-1,"chances")
+        bulls=0
+        cows=0
+        chances=chances-1
+print(num)
